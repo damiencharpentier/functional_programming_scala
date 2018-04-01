accf2505df32c1066b59bd0897b38b66da5fe59a
damiencharpentier <damien.charpentier@gmail.com>
2018-03-31 21:25:46 -0400

    7.2

--- src/main/scala/chapter_7/Par.scala
+++ src/main/scala/chapter_7/Par.scala
@@ -1,9 +1,28 @@
 package chapter_7
 
+import java.util.concurrent.{ExecutorService, Future, TimeUnit}
+
 sealed trait Par[A]
 
 object Par {
 
+  type Par[A] = ExecutorService => Future[A]
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = ???
+
+  def lazyAnit[A](a: A): Par[A] = fork(unit(a))
+
+  def fork[A](a: => Par[A]): Par[A] = ???
+
+  private case class UnitFuture[A](get: A) extends Future[A]
+ {
+   def isDone = true
+   def get(timeout: Long, units: TimeUnit) = get
+   def isCancelled = false
+   def cancel(evenIfRunning: Boolean): Boolean = false
+ }
   /**
     *
     * exercise 7.1
@@ -16,7 +35,13 @@ object Par {
     * @return
     */
   def map2Def[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] ={
-    ???
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get(),bf.get()))
+    }
   }
+
+
 
 }
